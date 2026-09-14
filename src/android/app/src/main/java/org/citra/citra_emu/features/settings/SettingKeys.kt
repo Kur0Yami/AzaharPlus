aabe476fad0c6cd70c9a8b58498dd43d36eeb650
Kur0Yami <133508919+Kur0Yami@users.noreply.github.com>
2026-09-14 13:49:33 +0700

    Macro: give it independent Button 1-5 slots, add D-Pad to combo/macro options

--- src/android/app/src/main/java/org/citra/citra_emu/features/settings/SettingKeys.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/features/settings/SettingKeys.kt
@@ -155,4 +155,9 @@ object SettingKeys {
     external fun combo_chain_timeout_ms(): String
     external fun macro_repeat_count(): String
     external fun macro_step_delay_ms(): String
+    external fun macro_button_buttons(): String
+    external fun macro_button_buttons_2(): String
+    external fun macro_button_buttons_3(): String
+    external fun macro_button_buttons_4(): String
+    external fun macro_button_buttons_5(): String
 }
