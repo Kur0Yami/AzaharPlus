7dd62ce03e71cc43d7442cc86ff149cdcf5eea95
Kur0Yami <133508919+Kur0Yami@users.noreply.github.com>
2026-09-15 12:05:49 +0700

    Add custom post processing shader support (Vulkan + Android import UI)

--- src/video_core/renderer_opengl/post_processing_opengl.cpp
+++ src/video_core/renderer_opengl/post_processing_opengl.cpp
@@ -2,17 +2,10 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#include <fstream>
-#include <sstream>
 #include <string>
 #include <vector>
-#include "common/common_paths.h"
-#include "common/file_util.h"
-#include "common/string_util.h"
+#include "video_core/post_processing_shader_loader.h"
 #include "video_core/renderer_opengl/post_processing_opengl.h"
-
-#include <boost/iostreams/device/file_descriptor.hpp>
-#include <boost/iostreams/stream.hpp>
 
 namespace OpenGL {
 
@@ -127,86 +120,16 @@ void SetOutput(float4 color_in)
 )";
 
 std::vector<std::string> GetPostProcessingShaderList(bool anaglyph) {
-    std::string shader_dir = FileUtil::GetUserPath(FileUtil::UserPath::ShaderDir);
-    std::vector<std::string> shader_names;
-
-    if (!FileUtil::IsDirectory(shader_dir)) {
-        FileUtil::CreateDir(shader_dir);
-    }
-
-    if (anaglyph) {
-        shader_dir = shader_dir + "anaglyph";
-        if (!FileUtil::IsDirectory(shader_dir)) {
-            FileUtil::CreateDir(shader_dir);
-        }
-    }
-
-    // Would it make more sense to just add a directory list function to FileUtil?
-    const auto callback = [&shader_names](u64* num_entries_out, const std::string& directory,
-                                          const std::string& virtual_name) -> bool {
-        const std::string physical_name = directory + DIR_SEP + virtual_name;
-        if (!FileUtil::IsDirectory(physical_name)) {
-            // The following is done to avoid coupling this to Qt
-            std::size_t dot_pos = virtual_name.rfind(".");
-            if (dot_pos != std::string::npos) {
-                if (Common::ToLower(virtual_name.substr(dot_pos + 1)) == "glsl") {
-                    shader_names.push_back(virtual_name.substr(0, dot_pos));
-                }
-            }
-        }
-        return true;
-    };
-
-    FileUtil::ForeachDirectoryEntry(nullptr, shader_dir, callback);
-
-    std::sort(shader_names.begin(), shader_names.end());
-
-    return shader_names;
+    return VideoCore::GetPostProcessingShaderList(anaglyph);
 }
 
 std::string GetPostProcessingShaderCode(bool anaglyph, std::string_view shader) {
-    std::string shader_dir = FileUtil::GetUserPath(FileUtil::UserPath::ShaderDir);
-    std::string shader_path;
-
-    if (anaglyph) {
-        shader_dir = shader_dir + "anaglyph";
-    }
-
-    // Examining the directory is done because the shader extension might have an odd case
-    // This can be eliminated if it is specified that the shader extension must be lowercase
-    const auto callback = [&shader, &shader_path](u64* num_entries_out,
-                                                  const std::string& directory,
-                                                  const std::string& virtual_name) -> bool {
-        const std::string physical_name = directory + DIR_SEP + virtual_name;
-        if (!FileUtil::IsDirectory(physical_name)) {
-            // The following is done to avoid coupling this to Qt
-            std::size_t dot_pos = virtual_name.rfind(".");
-            if (dot_pos != std::string::npos) {
-                if (Common::ToLower(virtual_name.substr(dot_pos + 1)) == "glsl" &&
-                    virtual_name.substr(0, dot_pos) == shader) {
-                    shader_path = physical_name;
-                    return false;
-                }
-            }
-        }
-        return true;
-    };
-
-    FileUtil::ForeachDirectoryEntry(nullptr, shader_dir, callback);
-    if (shader_path.empty()) {
+    const std::string shader_text =
+        VideoCore::GetPostProcessingShaderCodeRaw(anaglyph, shader);
+    if (shader_text.empty()) {
         return "";
     }
-
-    boost::iostreams::stream<boost::iostreams::file_descriptor_source> file;
-    FileUtil::OpenFStream<std::ios_base::in>(file, shader_path);
-    if (!file.is_open()) {
-        return "";
-    }
-
-    std::stringstream shader_text;
-    shader_text << file.rdbuf();
-
-    return dolphin_shader_header + shader_text.str();
+    return dolphin_shader_header + shader_text;
 }
 
 } // namespace OpenGL
